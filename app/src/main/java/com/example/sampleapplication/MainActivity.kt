1da879314a7d94588ac13052ee23438c28c3c37b
Sayyad Mahammad <sayyadmd62847@gmail.com>
2022-12-02 13:28:54 +0530

    added some pics

--- app/src/main/java/com/example/sampleapplication/MainActivity.kt
+++ app/src/main/java/com/example/sampleapplication/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.sampleapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var image = findViewById<ImageView>(R.id.imageView)
+        var btn = findViewById<Button>(R.id.button)
+        btn.setOnClickListener {
+            image.setImageResource(R.drawable.billions)
+        }
     }
 }
